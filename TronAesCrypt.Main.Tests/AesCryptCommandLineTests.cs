f026d487dbf269d22e1847621c6e113ad48d8257
tron <tron@tronsoft.nl>
2021-02-28 15:57:43 +0100

    Renamed TronAesCrypt.Tests to renamed TronAesCrypt.Core.Tests. Added TronAesCrypt.Main.Tests

---
+++ TronAesCrypt.Main.Tests/AesCryptCommandLineTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TronAesCrypt.Main.Tests
+{
+    [TestClass]
+    public class AesCryptCommandLineTests
+    {
+        [TestMethod]
+        public void FileIsEncrypted_Successfully()
+        {
+        }
+    }
+}
